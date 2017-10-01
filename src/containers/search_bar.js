8cdeb08dcdc6a7436e87baad37a40a51534225a4
steven4354 <steven4354@gmail.com>
2017-10-01 09:48:50 -0700

    Updated searchbar to accept state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component {
+  constructor(){
+    super(props)
+
+    this.state = {
+      term: ''
+    }
+
+    this.onInputChange.bind(this)
+  }
+
+  onInputChange(event){
+    console.log(event.target.value);
+    this.setState({
+      term: event.target.value
+    })
+  }
+
   render(){
     return(
       <form className="input-group">
-        <input />
+        <input
+          placeholder='Get a five-day forecast in your favorite cities'
+          className='form-control'
+          onChange={this.onInputChange}
+          />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
