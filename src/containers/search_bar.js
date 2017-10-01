3b6bcaf1b5735c911637e48b1553a02d242e9dd3
steven4354 <steven4354@gmail.com>
2017-09-30 22:04:18 -0700

    created initial searchbar file

---
+++ src/containers/search_bar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class SearchBar extends Component {
+  render(){
+    return(
+      <div className="form-group">
+        <input></input>
+        <span className="btn-secondary">Submit</span>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
