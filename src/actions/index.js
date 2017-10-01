e12b9d39e086e5b082d5fee0763063be9ff0c5e2
steven4354 <steven4354@gmail.com>
2017-10-01 11:44:35 -0700

    utilized axios for json request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
 const API_KEY = 'f2b68d4a6814fcda1ba804da9b490232';
+const ROOT_URL = "http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}"
+
+import axios from 'axios'
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city){
+  const url = '${ROOT_URL}&q=${city},us'
+  const request = axios.get(url)
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
